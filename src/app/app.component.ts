7982b6ce3f71f00d17a14dcadb56f25e3828fa93
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-08-11 23:31:57 +0100

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}} !</h1>`,
+  template: `<h1>Hello {{name}} !</h1>
+              <div>This is a tutorial in {{subject}}</div>`,
 })
 export class AppComponent  {
    name: string = 'Angular'; 
+   subject:string = 'AngularJS 2'
   }
