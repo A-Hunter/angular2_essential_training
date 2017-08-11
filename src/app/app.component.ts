88227d9c8c95677886a38736f4e5f652cef06eb3
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-08-11 21:37:22 +0100

    angularjs 2 documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+// A Component contains : 
+//    - Template : HTML
+//    - Class : Attributes and methods
+//    - Metadata : decorating and extending the functionnalities of the Class
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}} !</h1>`,
