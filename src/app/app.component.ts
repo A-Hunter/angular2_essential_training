77764eeb7e4fc3670a927d6ca6ff5d7eb2efa53d
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-08-12 00:15:10 +0100

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,19 @@ import { Component } from '@angular/core';
   template: `<h1>Hello {{name}} !</h1>
               <div>This is a tutorial in {{subject}}</div>`,
 })
+/**
+ @Component({
+  selector:'my-second-app',
+  templateUrl:'app/app.component.html'
+})
+export class AppCustomComponent{
+     attribute:string = '... maybe';
+}
+ */
+
+
 export class AppComponent  {
    name: string = 'Angular'; 
-   subject:string = 'AngularJS 2'
-  }
+   subject:string = 'AngularJS 2';
+}
+
