c4e7124b0b2b3b821c186f02311f8176ebcf1999
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-08-14 22:01:09 +0100

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
-
+/**
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}} !</h1>
               <div>This is a tutorial in {{subject}}</div>`,
 })
-/**
+*/
+
  @Component({
   selector:'my-second-app',
   templateUrl:'app/app.component.html'
 })
+/**
 export class AppCustomComponent{
      attribute:string = '... maybe';
 }
- */
-
-
+*/
 export class AppComponent  {
    name: string = 'Angular'; 
    subject:string = 'AngularJS 2';
+   bool:boolean = true;
 }
 
