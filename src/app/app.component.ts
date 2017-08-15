4dbaeb7d84698de863335193c2f0080cae1a19bb
Ghazi-Ennacer <ghazi.ennacer@gmail.com>
2017-08-15 22:38:09 +0100

    server lite npm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,7 @@
 import { Component } from '@angular/core';
-/**
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}} !</h1>
-              <div>This is a tutorial in {{subject}}</div>`,
+  template: `<h1>Hello {{name}}</h1>`,
 })
-*/
-
- @Component({
-  selector:'my-second-app',
-  templateUrl:'app/app.component.html'
-})
-/**
-export class AppCustomComponent{
-     attribute:string = '... maybe';
-}
-*/
-export class AppComponent  {
-
-  constructor(@Environment(‘test’ private sub:string){})
-
-   name: string = 'Angular'; 
-   subject:string = 'AngularJS 2';
-   bool:boolean = true;
-   list:any[] = [{
-      "id":1,
-      "name":"one"
-      },{
-        "id":"2",
-        "name":"two"
-      }];
-    images:any[] = [{
-        "id":"3",
-        "url":"app/images/3.jpg"
-      }];
-}
-
+export class AppComponent  { name = 'Angular'; }
